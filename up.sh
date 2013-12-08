95ac036ece591672a4c7fbdc4c7a836f4e8d6f2d
frotunato <frotunato@gmail.com>
2013-12-08 19:07:05 +0100

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,11 +1,8 @@
 #!bin/bash
 DIR="/home/fortuna/ramdisk"
-CURRENT="/home/fortuna/storage/current"
+CURRENT="/home/fortuna/storage/ramdisk"
 MINECRAFT="/home/fortuna/minecraft"
 cd "$MINECRAFT"
-mkdir $DIR/world
-mkdir $DIR/world_the_end
-mkdir $DIR/world_nether
 sudo rsync -r "$CURRENT/" "$DIR"
 sudo java -Xmx1700M -Xms1700M -jar spigot.jar
 
