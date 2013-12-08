fc6fbe911d5e5292f24992d4f668d5c501ac44bc
frotunato <frotunato@gmail.com>
2013-12-08 17:29:14 +0100

    Update fullBackupEnhanced.sh

--- fullBackupEnhanced.sh
+++ fullBackupEnhanced.sh
@@ -6,8 +6,11 @@ TIME="$(date +" [%H;%M;%S]")"
 CHECK_PID="$(pidof java)"
 SESSION="MINECRAFT_SERVER"
 if [ "$CHECK_PID" ]; then
-screen -S $SESSION -X stuff 'say Comenzando copia de seguridad...'`echo -ne '\015'`
+screen -S $SESSION -p 0 -X stuff 'say Comenzando copia de seguridad...'`echo -ne '\015'`
+START_TIME=`date +%s`
 tar cf "$BACKUP_DIR$DATE$TIME.tar.lz4" --use-compress-prog=lz4 "$CURRENT_BACKUP_DIR"
 find "$BACKUP_DIR" -mmin +240 -delete
-screen -S $SESSION -X stuff 'say Copia de seguridad completada'`echo -ne '\015'`
+END_TIME=`date +%s`
+let FINAL_TIME=$END_TIME-$START_TIME
+screen -S $SESSION -p 0 -X stuff "say Copia de seguridad completada en $FINAL_TIME segundo(s)"`echo -ne '\015'`
 fi
