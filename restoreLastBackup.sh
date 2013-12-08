4b0beb183c02e366a9b72f540fcf5e25d82c1e39
frotunato <frotunato@gmail.com>
2013-12-08 19:06:16 +0100

    Update restoreLastBackup.sh

--- restoreLastBackup.sh
+++ restoreLastBackup.sh
@@ -1,14 +1,17 @@
 #!bin/bash
-BACKUP_DIR="/home/fortuna/storage/backup"
-NEWEST_BACKUP="$(ls "$BACKUP_DIR" -rt | tail -1)"
 RAMDISK="/home/fortuna/ramdisk"
+BACKUP_DIR="/home/fortuna/storage/backup"
+CURRENT_BACKUP="/home/fortuna/storage/ramdisk"
+NEWEST_BACKUP="$(ls "$BACKUP_DIR" -rt | tail -1)"
 CHECK_PID="$(pidof java)"
+DIR="/home/fortuna/ramdisk"
 if [ ! "$CHECK_PID" ]; then
-sudo rm -rf /home/fortuna/ramdisk/* 
-sudo rm -rf /home/fortuna/storage/current/*
-sudo mkdir "$RAMDISK/world" "$RAMDISK/world_nether" "$RAMDISK/world_the_end"
+cd $RAMDISK
+sudo rm -r *
+cd $CURRENT_BACKUP
+sudo rm -r *
 sudo lz4 -d "$BACKUP_DIR/$NEWEST_BACKUP"
 TAR="$(find $BACKUP_DIR -type f -name '*.tar' -print)"
-sudo tar -xf "$TAR" ramdisk
+sudo tar -xf "$TAR" -C /home/fortuna/storage/ramdisk/
 sudo rm -f "$TAR"
 fi
