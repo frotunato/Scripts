33a4a9a582a305c2f8e57890d1230ee6c1dbccf3
frotunato <frotunato@gmail.com>
2013-12-08 16:17:36 +0100

    Create fullBackup.sh
    [NO RECOMMENDED] This script uses the 7zr utility and LMZA algorithm to compress the non-compressed backup called "current". Very high cpu usage, great filesize. 7zr can compress directories without using tar. It also removes the oldests backups.

---
+++ fullBackup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CURRENT_BACKUP_DIR="/home/fortuna/storage/current/*"
+BACKUP_DIR="/home/fortuna/storage/backup"
+DATE=$(date +"%d-%m-%Y-[%T]")
+CHECK_PID="$(pidof java)"
+if [ "$CHECK_PID" ]; then
+sudo 7zr a "$BACKUP_DIR/"$DATE.7z "$CURRENT_BACKUP_DIR"
+sudo find "$BACKUP_DIR" -mmin +240 -delete
+fi
