55b1b3bc2cc39a4e3ff9bf8534513b6abeb6667c
frotunato <frotunato@gmail.com>
2013-12-08 16:28:27 +0100

    Delete currentBackup.sh

--- currentBackup.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-DIR="/home/fortuna/ramdisk"
-CURRENT="/home/fortuna/storage/current"
-CHECK_EMPTY="$(ls -A $DIR)"
-CHECK_PID="$(pidof java)"
-if [ "$CHECK_PID" ]; then
-sudo rsync -ru "$DIR/" "$CURRENT"
-fi
-
