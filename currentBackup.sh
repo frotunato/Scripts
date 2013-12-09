db8dbf6b2faa37341fd55eb6d730e95e5c3f05a0
frotunato <frotunato@gmail.com>
2013-12-09 05:50:00 +0100

    Update currentBackup.sh

--- currentBackup.sh
+++ currentBackup.sh
@@ -1,10 +1,7 @@
 #!bin/bash
 DIR="/home/fortuna/ramdisk"
 CURRENT="/home/fortuna/storage/ramdisk"
-CHECK_EMPTY="$(ls -A $DIR)"
 CHECK_PID="$(pidof java)"
 if [ "$CHECK_PID" ]; then
-sudo rsync -ru "$DIR/" "$CURRENT"
+sudo rsync -ru --delete-delay  "$DIR/" "$CURRENT"
 fi
-
-
