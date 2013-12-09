f3807949d9e984895992fd055b99d490468c56b1
frotunato <frotunato@gmail.com>
2013-12-09 05:50:38 +0100

    Update fullBackupEnhanced.sh

--- fullBackupEnhanced.sh
+++ fullBackupEnhanced.sh
@@ -1,17 +1,21 @@
 #!bin/bash
 CURRENT_BACKUP_DIR="/home/fortuna/storage/ramdisk"
 BACKUP_DIR="/home/fortuna/storage/backup/"
-DATE="$(date +"%d-%m-%Y")"
-TIME="$(date +" [%H;%M;%S]")"
+DATE=$(date +"%d-%m-%Y [%T]")
 CHECK_PID="$(pidof java)"
 SESSION="MINECRAFT_SERVER"
 if [ "$CHECK_PID" ]; then
-screen -S $SESSION -p 0 -X stuff 'say Comenzando copia de seguridad...'`echo -ne '\015'`
+FIRST_TIME="$(date +" [%H:%M:%S]")"
+screen -S $SESSION -p 0 -X stuff "say $FIRST_TIME Comenzando copia de seguridad..."`echo -ne '\015'`
+screen -S $SESSION -p 0 -X stuff 'save-all'`echo -ne '\015'`
+sleep 10
 START_TIME=`date +%s`
 cd $CURRENT_BACKUP_DIR
-tar cf "$BACKUP_DIR$DATE$TIME.tar.lz4" --use-compress-prog=lz4 world world_nether world_the_end
+tar cf "$BACKUP_DIR$DATE.tar.lz4" --use-compress-prog=lz4 world world_nether world_the_end
 find "$BACKUP_DIR" -mmin +240 -delete
 END_TIME=`date +%s`
 let FINAL_TIME=$END_TIME-$START_TIME
-screen -S $SESSION -p 0 -X stuff "say Copia de seguridad completada en $FINAL_TIME segundo(s)"`echo -ne '\015'`
+LAST_TIME="$(date +" [%H:%M:%S]")"
+screen -S $SESSION -p 0 -X stuff "say $LAST_TIME Copia de seguridad completada en $FINAL_TIME segundo(s)"`echo -ne '\015'`
 fi
+
